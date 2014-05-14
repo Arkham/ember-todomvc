f9ca7e6c83a9c9a885e5c50600a871a3410c0a5e
Ju Liu <ju.liu@welaika.com>
2014-05-14 10:35:20 +0100

    Clear all completed todos

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -14,12 +14,26 @@ Todos.TodosController = Ember.ArrayController.extend({
       this.set('newTitle', '');
 
       todo.save();
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
 
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
 
   inflection: function() {
     var remaining = this.get('remaining');
